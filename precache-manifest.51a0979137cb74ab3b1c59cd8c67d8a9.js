ac64fdbfea708968bd60591aa2d18073650c8c5b
JiayuanDeng <JiayuanDeng0109@yahoo.com>
2018-11-26 19:01:21 -0800

    Updates

---
+++ precache-manifest.51a0979137cb74ab3b1c59cd8c67d8a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2cb553b44f708b70c787",
+    "url": "/static/js/main.2cb553b4.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "2cb553b44f708b70c787",
+    "url": "/static/css/main.268b8845.chunk.css"
+  },
+  {
+    "revision": "c9ab1e4603078bd8c17ff964c0cc14af",
+    "url": "/index.html"
+  }
+];
